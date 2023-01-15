628542c31be0155a9cca9c81004e8ab8300357bb
Blessmi K <kblessmi2001@gmail.com>
2023-01-15 19:02:57 +0530

    prime problem finished

---
+++ prime.js
@@ -0,0 +1,16 @@
+  let count=1;
+    for(let i=1;i<num;i++)
+    {
+        if(num%i==2)
+        {
+            count++
+        }
+    }
+    if(count==1)
+    {
+        console.log("Yes")
+    }
+    else
+    {
+        console.log("No")
+    }
